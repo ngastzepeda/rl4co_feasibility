e060ca358c30dd2766e6c9eb8bbd8982c8ef9fcf
Nayeli Gast Zepeda <n.gastzepeda@outlook.com>
2024-07-18 17:20:58 +0200

    tried different setup for logging

--- rl4co/utils/pylogger.py
+++ rl4co/utils/pylogger.py
@@ -3,53 +3,10 @@ import sys
 from lightning.pytorch.utilities.rank_zero import rank_zero_only
 from loguru import logger as loguru_logger
 
-
-def get_pylogger(name=__name__) -> logging.Logger:
-    """Initializes multi-GPU-friendly python command line logger."""
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-
-    # Remove any existing handlers
-    for handler in logger.handlers[:]:
-        logger.removeHandler(handler)
-
-    # Create handlers
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stderr_handler = logging.StreamHandler(sys.stderr)
-
-    # Set levels for handlers
-    stdout_handler.setLevel(logging.DEBUG)  # Handle DEBUG and above levels to INFO
-    stderr_handler.setLevel(logging.WARNING)  # Handle WARNING and above levels
-
-    # Create formatters and add them to the handlers
-    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s - %(message)s")
-    stdout_handler.setFormatter(formatter)
-    stderr_handler.setFormatter(formatter)
-
-    # Add handlers to the logger
-    logger.addHandler(stdout_handler)
-    logger.addHandler(stderr_handler)
-
-    # This ensures all logging levels get marked with the rank zero decorator
-    # otherwise logs would get multiplied for each GPU process in multi-GPU setup
-    logging_levels = (
-        "debug",
-        "info",
-        "warning",
-        "error",
-        "exception",
-        "fatal",
-        "critical",
-    )
-    for level in logging_levels:
-        setattr(logger, level, rank_zero_only(getattr(logger, level)))
-
-    return logger
-
-
-# Initialize the standard logger
-std_logger = get_pylogger()
+# use loguru for logging
+loguru_logger.remove()  # Remove any default handler if it exists
+loguru_logger.add(sys.stdout, level="INFO")  # Log INFO and above to stdout
+loguru_logger.add(sys.stderr, level="ERROR")  # Log ERROR and above to stderr
 
 
 # Intercept logs from the standard logging module to loguru
@@ -75,7 +32,48 @@ class InterceptHandler(logging.Handler):
 logging.root.handlers = []
 logging.basicConfig(handlers=[InterceptHandler()], level=0)
 
-# Now use loguru for logging
-loguru_logger.remove()  # Remove any default handler if it exists
-loguru_logger.add(sys.stdout, level="INFO")  # Log INFO and above to stdout
-loguru_logger.add(sys.stderr, level="ERROR")  # Log ERROR and above to stderr
+
+def get_pylogger(name=__name__) -> logging.Logger:
+    """Initializes multi-GPU-friendly python command line logger."""
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(InterceptHandler())
+    # logger.setLevel(logging.DEBUG)
+
+    # # Remove any existing handlers
+    # for handler in logger.handlers[:]:
+    #     logger.removeHandler(handler)
+
+    # # Create handlers
+    # stdout_handler = logging.StreamHandler(sys.stdout)
+    # stderr_handler = logging.StreamHandler(sys.stderr)
+
+    # # Set levels for handlers
+    # stdout_handler.setLevel(logging.DEBUG)  # Handle DEBUG and above levels to INFO
+    # stderr_handler.setLevel(logging.WARNING)  # Handle WARNING and above levels
+
+    # # Create formatters and add them to the handlers
+    # formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s - %(message)s")
+    # stdout_handler.setFormatter(formatter)
+    # stderr_handler.setFormatter(formatter)
+
+    # # Add handlers to the logger
+    # logger.addHandler(stdout_handler)
+    # logger.addHandler(stderr_handler)
+
+    # This ensures all logging levels get marked with the rank zero decorator
+    # otherwise logs would get multiplied for each GPU process in multi-GPU setup
+    logging_levels = (
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "exception",
+        "fatal",
+        "critical",
+    )
+    for level in logging_levels:
+        setattr(logger, level, rank_zero_only(getattr(logger, level)))
+
+    return logger
